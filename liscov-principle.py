b7dc39d8ad67f427f36d2980ca818bb7a098f4a4
IrinaVi <irinavik1805@gmail.com>
2022-10-16 19:45:11 +0100

    Add liskov subsititution principle

---
+++ liscov-principle.py
@@ -0,0 +1,41 @@
+# Created by Barbara Liskov in a 1987, this states that objects should be replaceable by their subtypes without altering how the program works.
+# In other words, derived classes must be substitutable for their base classes without causing errors.
+
+class Rectangle():
+    def __init__(self, width, height):
+        self._width = width
+        self._height = height
+
+    def get_width(self):
+        return self._width
+
+    def set_width(self, width):
+        self._width = width
+
+    def get_height(self):
+        return self._height
+
+    def set_height(self, height):
+        self._height = height
+
+    def area(self):
+        return self.get_width() * self.get_height()
+
+class Square(Rectangle):
+    def __init__(self, width):
+        self._width = width
+        self._height = width
+
+    def get_width(self):
+        return self._width
+
+    def set_width(self, width):
+        self._width = width
+        self._height = width
+
+    def get_height(self):
+        return self._height
+
+    def set_height(self, height):
+        self._height = height
+        self._width = height
